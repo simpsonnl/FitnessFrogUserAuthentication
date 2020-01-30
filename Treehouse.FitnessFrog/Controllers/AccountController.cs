de43dbdeeba1412816f66585a5829f767e6c8c8e
simpsonnl22@gmail.com <simpsonnl22@gmail.com>
2020-01-30 10:31:20 -0500

    added an accountRegister view model and an account controller.  also updated owin to not run on app startup in the root Web.config file.  also added a register view.

---
+++ Treehouse.FitnessFrog/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.ViewModels;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
