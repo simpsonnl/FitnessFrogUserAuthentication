8564a7ef9f8429cccfe49a72cca1f429237edaf1
simpsonnl22@gmail.com <simpsonnl22@gmail.com>
2020-01-30 10:44:10 -0500

    Added a user model and updated the context to inherit from IdentityDbContext.  Also added to the account controller.

--- Treehouse.FitnessFrog/Controllers/AccountController.cs
+++ Treehouse.FitnessFrog/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Treehouse.FitnessFrog.Shared.Models;
 using Treehouse.FitnessFrog.ViewModels;
 
 namespace Treehouse.FitnessFrog.Controllers
@@ -16,6 +17,22 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Register(AccountRegisterViewModel viewModel)
         {
+            // If the ModelState is valid...
+            if (ModelState.IsValid)
+            {
+                // Instantiate a User object
+                var user = new User { UserName = viewModel.Email, Email = viewModel.Email };
+
+                // Create the user
+
+                // If the user was successfully created...
+
+                // Sign-in the user and redirect them to the web app's "Home" page
+
+                // If there were errors...
+
+                // Add model errors
+            }
             return View(viewModel);
         }
     }
